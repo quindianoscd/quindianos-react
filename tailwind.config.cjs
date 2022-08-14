4351ba5100cb1177c95c5c4acde3f4ecdb385b48
Hernan Bermudez <61323223+HernanBermudez@users.noreply.github.com>
2022-08-13 22:37:44 -0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+      extend: {},
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
